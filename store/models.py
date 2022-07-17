28d24d82a2069a2f65b80affc8aaa4802b2c322d
Changhao <changhaowu00@gmail.com>
2022-07-17 12:24:20 +0200

    More one to many relationship

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+import collections
 import email
 from pyexpat import model
 from django.db import models
@@ -10,6 +11,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField()
     last_updeate = models.DateTimeField(auto_now_add=True)
+    collection=models.ForeignKey('Collection', on_delete=models.PROTECT)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -43,8 +45,23 @@ class Order(models.Model):
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES,default=PAYMENT_STATUS_PENDING
     )
+    customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete= models.PROTECT)
+    product = models.ForeignKey(Product, on_delete= models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price=models.DecimalField(max_digits=6,decimal_places=2)
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     Customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
