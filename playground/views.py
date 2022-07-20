f3366059509bfddc9dfb3957dba90c2f0b6338eb
Changhao <changhao.wu@m2c.es>
2022-07-20 17:10:46 +0200

    ex2.

--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from store.models import Product
 
 def say_hello(request):
-    query_set = Product.objects.filter(title__icontains='coffee')
+    query_set = Product.objects.filter(title__startswith='cofee')#endfswith
     return render(request,'hello.html',{'name':'Changhao','products':list(query_set)})
