fbca1151fae3c8fad05245ae776a35ba194ae527
Changhao <changhao.wu@m2c.es>
2022-07-20 17:07:02 +0200

    Another example

--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from store.models import Product
 
 def say_hello(request):
-    query_set = Product.objects.filter(unit_price__range = (20,30))
+    query_set = Product.objects.filter(collection__id__range=(1,2,3))
     return render(request,'hello.html',{'name':'Changhao','products':list(query_set)})
