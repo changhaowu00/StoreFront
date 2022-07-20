335b7845bb6ce2ad481ed0f754c496af63cfbf77
Changhao <changhao.wu@m2c.es>
2022-07-20 17:40:01 +0200

    reverse order

--- playground/views.py
+++ playground/views.py
@@ -5,5 +5,5 @@ from store.models import Product
 from django.db.models import Q,F
 
 def say_hello(request):
-    query_set = Product.objects.order_by('unit_price','-title')
+    query_set = Product.objects.order_by('unit_price','-title').reverse()
     return render(request,'hello.html',{'name':'Changhao','products':list(query_set)})
