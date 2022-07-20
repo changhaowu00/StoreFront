fcfad6da13d3cb31aa310f112a824adb3029ad26
Changhao <changhao.wu@m2c.es>
2022-07-20 17:50:46 +0200

    selecting fields

--- playground/views.py
+++ playground/views.py
@@ -5,6 +5,7 @@ from store.models import Product
 from django.db.models import Q,F
 
 def say_hello(request):
-    product = Product.objects.all()[5:10]
+    product = Product.objects.all().values('id','title','collection__title')#returns dictionaries
+    product = Product.objects.all().values('id','title','collection__title')# returns list of tuples
 
     return render(request,'hello.html',{'name':'Changhao','products':product})
