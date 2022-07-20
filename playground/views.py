8a94a5c2aa38ae822f6c4ab634ec4320fb0374b4
Changhao <changhao.wu@m2c.es>
2022-07-20 18:32:45 +0200

    Agregate function, max min avg , count

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 import imp
 from django.http import HttpResponse
 from django.shortcuts import render
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 from django.db.models import Q,F
+from django.db.models.aggregates import Count, Max,Min,Avg
 
 def say_hello(request):
-    qs= Product.objects.prefetch_related('collection').all()
-    return render(request,'hello.html',{'name':'Changhao','products':list(qs)})
+    result= Product.objects.aggregate(Count('id'),min_price = Min('unit_price'))
+    return render(request,'hello.html',{'name':'Changhao','result':result})
