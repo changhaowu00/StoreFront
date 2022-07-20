6525a946bc2a26462f67c16579207061145291f1
Changhao <changhao.wu@m2c.es>
2022-07-20 17:13:28 +0200

    ex4. Checking for null

--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from store.models import Product
 
 def say_hello(request):
-    query_set = Product.objects.filter(last_update__year=2021)
+    query_set = Product.objects.filter(description__isnull=True)
     return render(request,'hello.html',{'name':'Changhao','products':list(query_set)})
