8da10c245d146789635ec40d63cfae06d18fe923
Changhao <changhao.wu@m2c.es>
2022-07-20 20:29:52 +0200

    Custom manager

--- playground/views.py
+++ playground/views.py
@@ -10,16 +10,13 @@ from django.db.models.aggregates import Count, Max,Min,Avg
 
 from django.contrib.contenttypes.models import ContentType
 from store.models import Product
+from django.db import models
+
 from tags.models import TaggedItem
 
+
 def say_hello(request):
-    #getting tag of a product
-    content_type = ContentType.objects.get_for_models(Product)
-    qs = TaggedItem.objects\
-        .select_related('tag')\
-        .filter(
-            content_type = content_type,
-            object_id = 1
-        )
+
+    qs = TaggedItem.objects.get_tags_for(Product,1)
 
     return render(request,'hello.html',{'name':'Changhao','tags': list(qs)})
