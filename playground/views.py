17b29de0900de532ea58bfef5b39db560e1f29d2
Changhao <changhao.wu@m2c.es>
2022-07-20 19:37:19 +0200

    Annotate method

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,12 @@
 import imp
+from multiprocessing.sharedctypes import Value
 from django.http import HttpResponse
 from django.shortcuts import render
-from store.models import Product, OrderItem, Order
-from django.db.models import Q,F
+from store.models import Customer, Product, OrderItem, Order
+from django.db.models import Q,F,Value
 from django.db.models.aggregates import Count, Max,Min,Avg
 
 def say_hello(request):
-    result= Product.objects.aggregate(Count('id'),min_price = Min('unit_price'))
+    Customer.objects.annotate(new_id = Value(True))
+    Customer.objects.annotate(new_id = F('id')+1)
     return render(request,'hello.html',{'name':'Changhao','result':result})
