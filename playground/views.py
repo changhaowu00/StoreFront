529ea167227770402b049f7c39d7115759fa8f24
Changhao <changhao.wu@m2c.es>
2022-07-20 17:37:22 +0200

    F object

--- playground/views.py
+++ playground/views.py
@@ -5,5 +5,5 @@ from store.models import Product
 from django.db.models import Q,F
 
 def say_hello(request):
-    query_set = Product.objects.filter(inventory = F('unit_price'))
+    query_set = Product.objects.filter(inventory = F('collection__id'))
     return render(request,'hello.html',{'name':'Changhao','products':list(query_set)})
