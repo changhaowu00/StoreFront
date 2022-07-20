ef71c5b4876255a6a7880639451f237f400fd2d9
Changhao <changhao.wu@m2c.es>
2022-07-20 17:36:09 +0200

    F object

--- playground/views.py
+++ playground/views.py
@@ -2,9 +2,8 @@ import imp
 from django.http import HttpResponse
 from django.shortcuts import render
 from store.models import Product
-from django.db.models import Q
+from django.db.models import Q,F
 
 def say_hello(request):
-    #Products: inventory < 10 AND not price <20
-    query_set = Product.objects.filter(Q(inventory__tl=10) & ~Q(unit_price__lt=20))
+    query_set = Product.objects.filter(inventory = F('unit_price'))
     return render(request,'hello.html',{'name':'Changhao','products':list(query_set)})
