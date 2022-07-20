33f9629f683ae33518e87069e95f085f07b4665d
Changhao <changhao.wu@m2c.es>
2022-07-20 17:11:42 +0200

    ex3.

--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from store.models import Product
 
 def say_hello(request):
-    query_set = Product.objects.filter(title__startswith='cofee')#endfswith
+    query_set = Product.objects.filter(last_update__year=2021)
     return render(request,'hello.html',{'name':'Changhao','products':list(query_set)})
