2d4effd8032c1c4f9bba0891918a4a62793b4744
Changhao <changhao.wu@m2c.es>
2022-07-20 20:00:39 +0200

    Working with Expresion Wraper

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,17 @@
 import imp
 from multiprocessing.sharedctypes import Value
+from django.forms import DecimalField
 from django.http import HttpResponse
 from django.shortcuts import render
 from store.models import Customer, Product, OrderItem, Order
-from django.db.models import Q,F,Value,Func
+from django.db.models import Q,F,Value,Func, ExpressionWrapper
 from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Max,Min,Avg
 
 def say_hello(request):
-
+    discounter_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
     queryset = Customer.objects.annotate(
-        orders_count = Count('order')
+        discounted_price  = discounter_price
     )
 
 
