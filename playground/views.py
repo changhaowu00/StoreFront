04bb0bc120f2bb2a57651cff6b5b18d6d9f66804
Changhao <changhao.wu@m2c.es>
2022-07-20 17:45:54 +0200

    5,6,7,8,9

--- playground/views.py
+++ playground/views.py
@@ -5,6 +5,6 @@ from store.models import Product
 from django.db.models import Q,F
 
 def say_hello(request):
-    product = Product.objects.all()[:5]
+    product = Product.objects.all()[5:10]
 
     return render(request,'hello.html',{'name':'Changhao','products':product})
