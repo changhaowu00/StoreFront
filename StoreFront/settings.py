a4ecbd65c6e6a7e392499529b935edcb9d486aeb
Changhao <changhao.wu@m2c.es>
2022-07-20 17:04:11 +0200

    Managers and QuerySets. Retrieving Objects. Filtering Objects

--- StoreFront/settings.py
+++ StoreFront/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'StoreFront', 
-        'USER': 'postgres', 
+        'USER': 'storefront', 
         'PASSWORD': 'postgre',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from store.models import Product
 
 def say_hello(request):
-    return render(request,'hello.html',{'name':'Changhao'})
+    query_set = Product.objects.filter(unit_price__range = (20,30))
+    return render(request,'hello.html',{'name':'Changhao','products':list(query_set)})
