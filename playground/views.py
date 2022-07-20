2d414cd889238ca9787e34cd343130b00d77f2de
Changhao <changhao.wu@m2c.es>
2022-07-20 19:49:26 +0200

    Calling Database Functions Check documentation django database functions

--- playground/views.py
+++ playground/views.py
@@ -3,10 +3,25 @@ from multiprocessing.sharedctypes import Value
 from django.http import HttpResponse
 from django.shortcuts import render
 from store.models import Customer, Product, OrderItem, Order
-from django.db.models import Q,F,Value
+from django.db.models import Q,F,Value,Func
+from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Max,Min,Avg
 
 def say_hello(request):
-    Customer.objects.annotate(new_id = Value(True))
-    Customer.objects.annotate(new_id = F('id')+1)
-    return render(request,'hello.html',{'name':'Changhao','result':result})
+
+    queryset = Customer.objects.annotate(
+        #CONCAT
+        fullname = Func(
+            F('first_name'),Value(' '),F('last_name'),function='CONCAT'
+        )
+    )
+
+    queryset = Customer.objects.annotate(
+        #CONCAT
+        fullname = Concat(
+            'first_name',Value(' '),'last_name'
+        )
+    )
+
+
+    return render(request,'hello.html',{'name':'Changhao'})
