775d4e40fd0b041709e282ae0942f5fde9e4eea2
Changhao <changhao.wu@m2c.es>
2022-07-20 17:25:54 +0200

    Another example Q object

--- playground/views.py
+++ playground/views.py
@@ -5,6 +5,6 @@ from store.models import Product
 from django.db.models import Q
 
 def say_hello(request):
-    #Products: inventory < 10 OR price <20
-    query_set = Product.objects.filter(Q(inventory__tl=10) | Q(unit_price__lt=20))
+    #Products: inventory < 10 AND not price <20
+    query_set = Product.objects.filter(Q(inventory__tl=10) & ~Q(unit_price__lt=20))
     return render(request,'hello.html',{'name':'Changhao','products':list(query_set)})
