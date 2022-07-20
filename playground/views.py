e422147ce802b21fd8b56a13814957535cbe0bd8
Changhao <changhao.wu@m2c.es>
2022-07-20 18:11:20 +0200

    Prefetch, for many to many

--- playground/views.py
+++ playground/views.py
@@ -5,5 +5,5 @@ from store.models import Product, OrderItem
 from django.db.models import Q,F
 
 def say_hello(request):
-    qs= Product.objects.select_related('collection').all()
+    qs= Product.objects.prefetch_related('collection').all()
     return render(request,'hello.html',{'name':'Changhao','products':list(qs)})
