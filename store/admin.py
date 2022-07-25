27d7d57d8ad6310d47978b7028d1d2759eddf1d5
Changhao Wu <changhaowu00@gmail.com>
2022-07-25 14:12:37 +0200

    Adding Search to the List PAge

--- store/admin.py
+++ store/admin.py
@@ -38,6 +38,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name','last_name']
     list_per_page = 10
+    search_fields = ['first_name__istartswith','last_name__istartwith']
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
