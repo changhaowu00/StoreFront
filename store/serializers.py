adb3f9f05760478c51931d2b5d0cd160ab919654
Changhao Wu <changhaowu00@gmail.com>
2022-07-25 18:13:47 +0200

    Serializing Relationships

--- store/serializers.py
+++ store/serializers.py
@@ -1,15 +1,29 @@
-from itertools import product
-import profile
+
+import collections
 from turtle import title
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 from decimal import Decimal
+from store.models import Product, Collection
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
 
 class ProductSerializer(serializers.Serializer):
     id =serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6,decimal_places=2,source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # collection = serializers.PrimaryKeyRelatedField(
+    #     queryset=Collection.objects.all()
+    # )
+    # collection = serializers.StringRelatedField(
+    #     queryset=Collection.objects.all()
+    # )
+
+    collection = CollectionSerializer()
+
 
     def calculate_tax(self,product):
         return product.unit_price * Decimal(1.1)
