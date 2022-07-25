e638d61a9f7e7eca33789265bcd5119424a47980
Changhao Wu <changhaowu00@gmail.com>
2022-07-25 14:50:18 +0200

    Data Validation Doc"Django Validator"

--- store/models.py
+++ store/models.py
@@ -5,6 +5,8 @@ from turtle import title
 from django.db import models
 from django.forms import model_to_dict
 
+from django.core.validators import MinValueValidator
+
 # Create your models here.
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -25,12 +27,16 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField() #Help Search engine to find our product
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+    description = models.TextField(null=True,blank=True)
+    unit_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(1)])
+
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now_add=True)
     collection=models.ForeignKey('Collection', on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion)
+    promotions = models.ManyToManyField(Promotion,blank=True)
 
     
     def __str__(self) -> str:
