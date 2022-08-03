71a52db644ed9765a279ecac5284bb9d15ec3224
Changhao Wu <changhaowu00@gmail.com>
2022-08-03 12:20:50 +0200

    13-Sorting

--- store/views.py
+++ store/views.py
@@ -14,15 +14,16 @@ from rest_framework.mixins import ListModelMixin,CreateModelMixin
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend,SearchFilter]
+    filter_backends = [DjangoFilterBackend,SearchFilter, OrderingFilter]
     # filterset_fields = ['collection_id']
     filterset_class = ProductFilter
     search_fields = ['title','description']
+    ordering_fields = ['unit_price','last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
