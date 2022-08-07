dc10eb2964eee7b35e8a1ea55c2b27bf0c14cdad
Changhao <changhaowu00@gmail.com>
2022-08-07 11:18:53 +0200

    5-Getting a Cart

--- store/serializers.py
+++ store/serializers.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from itertools import product
 from store.models import Cart, CartItem, Product, Collection, Review
 from rest_framework import serializers
 
@@ -32,9 +33,29 @@ class ReviewSerializer(serializers.ModelSerializer):
         product_id = self.context['product_id']
         return Review.objects.create(product_id=product_id, **validated_data)
 
+class SimpleProdutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','unit_price']
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProdutSerializer()
+    total_price = serializers.SerializerMethodField()
+
+
+    def get_total_price(self,cart_item:CartItem):
+        return cart_item.quantity * cart_item.product.unit_price
+    class Meta:
+        model = CartItem
+        fields = ['id','product','quantity','total_price']
 
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many = True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self,cart):
+        return sum([item.quantity * item.product.unit_price for item in cart.item.all()].all())
     class Meta:
         model = Cart
-        fields = ['id']
+        fields = ['id','items']
