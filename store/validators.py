1b15699ff662948077988b3fe87f58c762b22bcd
Changhao Wu <changhaowu00@gmail.com>
2022-08-16 12:31:23 +0200

    8- Enabling CORS. 'pipenv install django-cors-headers'

--- store/validators.py
+++ store/validators.py
@@ -4,7 +4,7 @@ from xml.dom import ValidationErr
 from django.core.exceptions import ValidationError
 
 def validate_file_size(file):
-    max_size_KB = 50
+    max_size_KB = 500
 
     if file.size > max_size_KB*1024:
         raise ValidationError(f'files cannot be larger then {max_size_KB} KB!')
